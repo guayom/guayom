9fd8c42e333ae62bde9302287b99b52e87d74b2a
guayom <guayo.mena@gmail.com>
2019-01-25 10:11:48 -0600

    Try setting height to 100vh

--- src/components/global/grids.js
+++ src/components/global/grids.js
@@ -4,8 +4,7 @@ const MainGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: 1fr 60px;
-  height: 100%;
-  min-height: calc(100vh - 60px);
+  height: 100vh;
 
   .navigation {
     order: 2;
