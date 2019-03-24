9c83b26841087a364082ebd39ad68d91a90af8df
guayom <guayo.mena@gmail.com>
2019-03-24 12:06:25 -0600

    Refactor gatsby-node to make page creation async

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,34 @@
-const path = require("path")
+const path = require('path')
 
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
-
-  return graphql(`
+async function createRegularPages(graphql, actions, reporter) {
+  const { createPage, createPageDependency } = actions
+  const result = await graphql(`
     {
       pages: allSanityPage {
         edges {
           node {
             id
-            path
+            pagePath: path
           }
         }
       }
     }
-  `).then(result => {
-    if (result.errors) {
-      return Promise.reject(result.errors)
-    }
+  `)
 
-    result.data.pages.edges.forEach(({ node }) => {
-      const template = path.resolve(`src/templates/main.js`)
-      createPage({
-        path: node.path,
-        component: template,
-        context: { id: node.id },
-      })
-    })
+  if (result.errors) throw result.errors
+
+  const pageEdges = (result.data.pages || {}).edges || []
+
+  pageEdges.forEach((edge, index) => {
+    const { id, pagePath } = edge.node
+    const template = path.resolve(`src/templates/main.js`)
+
+    reporter.info(`Creating page: ${pagePath}`)
+    createPage({ path: pagePath, component: template, context: { id } })
+    createPageDependency({ path: pagePath, nodeId: id })
   })
+}
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  await createRegularPages(graphql, actions, reporter)
 }
