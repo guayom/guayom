669a7da225334a5f2e32e1f9f622d8cc4f782c62
guayom <guayo.mena@gmail.com>
2019-03-28 09:31:25 -0600

    Uninstall gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,14 +25,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: "markdown-pages",
-      },
-    },
-    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
